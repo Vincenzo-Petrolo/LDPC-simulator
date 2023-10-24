40a3587ccef0315ca54a8827d0b91ee63c7da38c
Vincenzo Petrolo <vincenzo.petrolo99@gmail.com>
2023-10-24 17:41:30 +0200

    feat : more generic codeword gen

--- codeword_gen.py
+++ codeword_gen.py
@@ -4,9 +4,11 @@ import random
 class Code(object):
     def __init__(self, adjmatr_file) -> None:
         self.H = np.loadtxt(adjmatr_file, dtype=int)
-        self.G = np.hstack([ np.eye(3),self.H[:,:3]])
+        shape = self.H.shape
+        self.G = np.hstack([ np.eye(shape[0]),self.H[:,:shape[1]-shape[0]]])
     
     def codeword(self) -> list:
-        seed = [random.choice([0,1]) for _ in range(3)]
+        shape = self.H.shape
+        seed = [random.choice([0,1]) for _ in range(shape[0])]
         
         return (seed @ self.G) % 2
